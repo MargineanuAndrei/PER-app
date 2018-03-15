2fe8498793bdfbf861e925ecff4a9d126de7a83c
MargineanuAndrei <margineanuandrei94@gmail.com>
2018-03-15 14:32:59 +0200

    add some styles to home route with

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 class Header extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/movie">Movie</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarColor03">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link to="/" className="nav-link" >Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/create" className="nav-link">Add Movie</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -14,7 +14,6 @@ class Home extends Component {
     axios.get('http://localhost:5000/movies')
       .then(response => {
         const movies = response.data.data;
-        console.log(movies);
         this.setState({ movies });
       });
   }
@@ -25,19 +24,34 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Home sweet home</h2>
-
-        {
+      <div className="container">
+        <h3 className="text-success my-header">Available Movies</h3>
+          <div className="col-lg-12">
+            <div className="row">
+            {
             this.state.movies.map(movie => {
               return(
-                <div key={movie.id}>
-                  <div>{movie.title}</div>
+                <div key={movie.id} className="col-lg-4 card-container">
+                  <div key={movie.id} className="card border-primary mb-3 card-content">
+                    <div className="card-header">
+                      {movie.title}
+                    </div>
+                    <div className="card-body">
+                      <p className="card-text">
+                        <span className="badge badge-success"><strong>Movie Rating: {movie.rating}</strong></span><br/>
+                        {movie.description}
+                      </p>
+                    </div>
+                    <div className="card-footer text-muted button-wraper">
+                      <button type="button" className="btn btn-secondary costum-button">Details</button>
+                    </div>
+                  </div>
                 </div>
               );
             })
-        }
-
+          }
+          </div>
+        </div>
       </div>
     );
   }
