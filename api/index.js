25cfd4e0ddd8dade9a15d65cc77bc3ab3567c483
MargineanuAndrei <margineanuandrei94@gmail.com>
2018-03-15 17:21:19 +0200

    add functionality to consume delete route

--- api/index.js
+++ api/index.js
@@ -6,10 +6,23 @@ const router = express.Router();
 const movieRoute = require('./routes/movies.js');
 
 // Default midlleware to allow client side calls to api
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "*");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 // Default midlleware for body-parser
--- front/src/components/Movie.js
+++ front/src/components/Movie.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from "react-router-dom";
 
 class Movie extends Component {
   constructor(props){
     super(props);
     this.state = {
-      movie: {}
+      movie: {},
+      redirect: false
     };
+    this.deleteMovie = this.deleteMovie.bind(this);
   }
 
   fetchOneMovie(){
@@ -21,19 +24,40 @@ class Movie extends Component {
       });
   }
 
+  deleteMovie(){
+    const id = this.props.match.params.id;
+    axios({
+      method: 'delete',
+      url: `http://localhost:5000/movies/${id}`,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(() => this.setState({ redirect: true }))
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   componentWillMount(){
     this.fetchOneMovie();
   }
 
   render() {
+    const { redirect, movie } = this.state;
+
+     if (redirect) {
+       return <Redirect to='/'/>;
+     }
     return (
       <div className="container container-wraper">
         <div className="col-lg-12">
           <div className="row">
             <div className="col-lg-6">
-              <h3 className="display-5">{this.state.movie.title}</h3>
-              <p className="lead">{this.state.movie.description}</p>
-              <button type="button" className="btn btn-danger">Delete</button>
+              <h3 className="display-5">{movie.title}</h3>
+              <p className="lead">{movie.description}</p>
+              <button type="button" className="btn btn-danger" onClick={this.deleteMovie}>Delete</button>
             </div>
           </div>
         </div>
